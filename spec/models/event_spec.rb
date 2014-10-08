c14c61ea2ac2efa8799a939c7329f295493a2f3a
ta1kt0me <p.wadachi@gmail.com>
2014-10-08 13:40:46 +0900

    modify shoulda-matchers format

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,13 +2,7 @@ require 'rails_helper'
 
 describe Event do
   describe '#name' do
-    context '空白のとき' do
-      let(:event) { Event.new(name: '') }
-
-      it 'validでないこと' do
-        event.valid?
-        expect(event.errors[:name]).to be_present
-      end
-    end
+    it { should validate_presence_of(:name) }
+    it { should ensure_length_of(:name).is_at_most(50) }
   end
 end
