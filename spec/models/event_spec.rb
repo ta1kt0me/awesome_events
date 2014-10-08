437f93b6394b6c132ae26cdd5bb63a0f6aadbe5e
ta1kt0me <p.wadachi@gmail.com>
2014-10-08 14:30:18 +0900

    use factorygirl in events spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,4 +5,19 @@ describe Event do
     it { should validate_presence_of(:name) }
     it { should ensure_length_of(:name).is_at_most(50) }
   end
+
+  describe '#created_by' do
+    let(:event) { create(:event) }
+    subject { event.created_by?(user) }
+
+    context '引数が nil な時' do
+      let(:user) { nil }
+      it { should be_falsey }
+    end
+
+    context '#owner_id と 引数の#id が同じ時' do
+      let(:user) { double('user', id: event.id) }
+      it { should be_truthy }
+    end
+  end
 end
